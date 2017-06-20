63119dc847125329b1c4f7344725f5335fceebe3
Alex Lapinski <contact@alexlapinski.name>
2017-06-19 21:38:17 -0400

    Added base board with get/set

---
+++ index.js
@@ -0,0 +1,19 @@
+const Board = require('./model');
+
+main();
+
+function main() {
+
+    const board = new Board();
+    console.log('Initial State');
+    console.log(board.toString());
+    console.log();
+
+    board.setTile(1, 1, -1);
+    board.setTile(0, 1, 1);
+    board.setTile(2, 2, -1);
+    board.setTile(0, 0, 1);
+
+    console.log('After 3 turns');
+    console.log(board.toString());
+}
