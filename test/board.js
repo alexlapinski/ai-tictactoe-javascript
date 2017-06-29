536a00bc8a07c5139971a127c34eaecbb63f5788
Alex Lapinski <contact@alexlapinski.name>
2017-06-28 21:58:59 -0400

    Added more tests for 100% coverage

--- test/board.js
+++ test/board.js
@@ -1,4 +1,5 @@
 const {expect} = require('chai');
+const _ = require('lodash');
 const Board = require('../models/board');
 
 
@@ -141,6 +142,149 @@ describe('Board', () => {
 
             expect(subject.checkWinner()).to.equal(Board.EMPTY_CELL);
         });
+
+        describe('horizontal winner', () => {
+            const maxRows = 3;
+            const players = [Board.X_CELL, Board.O_CELL];
+
+            _.each(players, (player) => {
+                for( let row = 0; row < maxRows; row++ ) {
+                    it(`Board should have winner as ${player} when horizontal win exists in row ${row}.`, () => {
+                        const subject = new Board();
+
+                        _.each(_.range(subject.height), (col) => subject.setTile(col, row, player));
+
+                        expect(subject.checkWinner()).to.equal(player)
+                    });
+                }
+            });
+
+            it(`should have ${Board.X_CELL} as winner when realistic row win exists.`, () => {
+                const subject = new Board();
+
+                subject.setTile(0, 0, Board.X_CELL);
+                subject.setTile(0, 1, Board.O_CELL);
+                subject.setTile(1, 0, Board.X_CELL);
+                subject.setTile(0, 2, Board.O_CELL);
+                subject.setTile(2, 0, Board.X_CELL);
+
+                expect(subject.checkWinner()).to.equal(Board.X_CELL);
+            });
+
+            it(`should have ${Board.O_CELL} as winner when realistic row win exists.`, () => {
+                const subject = new Board();
+
+                subject.setTile(1, 1, Board.X_CELL);
+                subject.setTile(0, 0, Board.O_CELL);
+                subject.setTile(2, 2, Board.X_CELL);
+                subject.setTile(1, 0, Board.O_CELL);
+                subject.setTile(0, 1, Board.X_CELL);
+                subject.setTile(2, 0, Board.O_CELL);
+
+                expect(subject.checkWinner()).to.equal(Board.O_CELL);
+            });
+        });
+
+        describe('vertical winner', () => {
+            const maxCols = 3;
+            const players = [Board.X_CELL, Board.O_CELL];
+
+            _.each(players, (player) => {
+                for( let col = 0; col < maxCols; col++ ) {
+                    it(`Board should have winner as ${player} when vertical win exists in col ${col}.`, () => {
+                        const subject = new Board();
+
+                        _.each(_.range(subject.height), (row) => subject.setTile(col, row, player));
+
+                        expect(subject.checkWinner()).to.equal(player)
+                    });
+                }
+            });
+
+            it(`should have ${Board.X_CELL} as winner when realistic col win exists.`, () => {
+                const subject = new Board();
+
+                subject.setTile(0, 0, Board.X_CELL);
+                subject.setTile(1, 0, Board.O_CELL);
+                subject.setTile(0, 1, Board.X_CELL);
+                subject.setTile(2, 0, Board.O_CELL);
+                subject.setTile(0, 2, Board.X_CELL);
+
+                expect(subject.checkWinner()).to.equal(Board.X_CELL);
+            });
+
+            it(`should have ${Board.O_CELL} as winner when realistic col win exists.`, () => {
+                const subject = new Board();
+
+                subject.setTile(1, 1, Board.X_CELL);
+                subject.setTile(0, 0, Board.O_CELL);
+                subject.setTile(2, 2, Board.X_CELL);
+                subject.setTile(0, 1, Board.O_CELL);
+                subject.setTile(1, 2, Board.X_CELL);
+                subject.setTile(0, 2, Board.O_CELL);
+
+                expect(subject.checkWinner()).to.equal(Board.O_CELL);
+            });
+        });
+
+        describe('diagonal winner', () => {
+            const players = [Board.X_CELL, Board.O_CELL];
+            _.each(players, (player) => {
+                it(`Board should have winner as ${player} when diagonal win exists.`, () => {
+                    const subject = new Board();
+
+                    subject.setTile(0, 0, player);
+                    subject.setTile(1, 1, player);
+                    subject.setTile(2, 2, player);
+
+                    expect(subject.checkWinner()).to.equal(player)
+                });
+            });
+
+            it(`should have ${Board.X_CELL} as winner when realistic diagonal win exists.`, () => {
+                const subject = new Board();
+
+                subject.setTile(0, 0, Board.X_CELL);
+                subject.setTile(0, 1, Board.O_CELL);
+                subject.setTile(1, 1, Board.X_CELL);
+                subject.setTile(1, 0, Board.O_CELL);
+                subject.setTile(2, 2, Board.X_CELL);
+
+                expect(subject.checkWinner()).to.equal(Board.X_CELL);
+            });
+
+            it(`should have ${Board.O_CELL} as winner when realistic diagonal win exists.`, () => {
+                const subject = new Board();
+
+                subject.setTile(0, 1, Board.X_CELL);
+                subject.setTile(0, 0, Board.O_CELL);
+                subject.setTile(1, 0, Board.X_CELL);
+                subject.setTile(1, 1, Board.O_CELL);
+                subject.setTile(0, 2, Board.X_CELL);
+                subject.setTile(2, 2, Board.O_CELL);
+
+                expect(subject.checkWinner()).to.equal(Board.O_CELL);
+            });
+        });
+
+    });
+
+    describe('#toString()', () => {
+       it('Board should be written to a string', () => {
+          const subject = new Board();
+
+          const initialBoard = '   0.000    0.000    0.000\n   0.000    0.000    0.000\n   0.000    0.000    0.000';
+          expect(subject.toString()).to.equal(initialBoard);
+
+          const afterFirstMove = '   0.000    0.000    0.000\n   0.000   -1.000    0.000\n   0.000    0.000    0.000';
+          subject.setTile(1, 1, -1);
+          expect(subject.toString()).to.equal(afterFirstMove);
+
+           const afterSecondMove = '   1.000    0.000    0.000\n   0.000   -1.000    0.000\n   0.000    0.000    0.000';
+           subject.setTile(0, 0, 1);
+           expect(subject.toString()).to.equal(afterSecondMove);
+
+       });
     });
 
 });
